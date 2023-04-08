9d407e2e09dc5201dbccbcb419ed26be77bc43ff
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-04-08 09:18:52 +0300

    This is as far as I could come for making the cart items dynamic
    opened new branch for this to see how it will change when I watch the videos too

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartContextProvider from "./store/CartContextProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
   }
 
   return (
-    <>
+    <CartContextProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler}/>
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartContextProvider>
   );
 }
 
