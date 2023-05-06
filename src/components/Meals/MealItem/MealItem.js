145677470dcab030a0dea3e4d120ca96f2017abe
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-05-06 12:08:21 +0300

    Taking an error here, will try to solve it
    Error is about cart functionality

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 function MealItem(props) {
 
+    const cartCtx = useContext(CartContext);
+
     const meal = props.meal;
     const price = `$${meal.price.toFixed(2)}`
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
 
     return (
         <li className={classes.meal}>
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import classes from "./Input.module.css";
 
-function Input(props) {
-
+const Input = React.forwardRef((props, ref) => {
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}> {props.label} </label>
             <input {...props.input} />
         </div>
-    )
-}
+    );
+});
+
+
+// This is one of the few examples where I couldn't create a component using both approaches,
+// How to wrap function with forwardRef ?
+// function Input(props) {
+
+//     return (
+//         <div className={classes.input}>
+//             <label htmlFor={props.input.id}> {props.label} </label>
+//             <input {...props.input} />
+//         </div>
+//     )
+// }
 
 export default Input;
