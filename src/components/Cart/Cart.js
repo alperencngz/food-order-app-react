3aacbaa6e3f936e75cd31b6de884c8722b54b20d
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-05-07 10:34:17 +0300

    Added better Reducer logic and remove functionality
    Looking good so far

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,11 +15,11 @@ function Cart(props) {
     console.log(cartCtx.items);
 
     const cartItemRemoveHandler = id => {
-
+        cartCtx.removeItem(id);
     };
 
-    const cartItemAddHandler = id => {
-
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({...item, amount:1})
     };
 
     const cartItems = (
